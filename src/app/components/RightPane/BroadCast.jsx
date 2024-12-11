7a9ea832676897ac30d05135f28271df82955a95
Lakith Randula <110283259+GLRandula@users.noreply.github.com>
2024-12-11 17:43:04 +0530

    New changes

--- src/app/components/RightPane/BroadCast.jsx
+++ src/app/components/RightPane/BroadCast.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import useSupabase from "@/app/hooks/useSupabase";
 
--- src/app/components/RightPane/ReceiveMsg.jsx
+++ src/app/components/RightPane/ReceiveMsg.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import useSupabase from "@/app/hooks/useSupabase";
 
