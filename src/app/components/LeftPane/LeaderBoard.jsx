1e490f500bed1f40524d865d79c497a15161bda7
PinsaraPerera <1pawanpinsara@gmail.com>
2024-12-11 14:18:33 +0530

    listen from database

---
+++ src/app/components/LeftPane/LeaderBoard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useSupabase from "@/app/hooks/useSupabase";
+
+const LeaderBoard = () => {
+  const supabase = useSupabase();
+
+  supabase.channel('room1')
+  .on('postgres_changes', { event: '*', schema: 'public', table: 'demo-table' }, payload => {
+    console.log('Change received!', payload)
+  })
+  .subscribe()
+
+
+  return <div>LeaderBoard</div>;
+};
+
+export default LeaderBoard;
