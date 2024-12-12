e55ddc5be11d69cbcb8d420c56f3944467c5ffc7
Tharindu209 <tharindudamruwan23@gmail.com>
2024-12-13 01:08:39 +0530

    typing box fixed

--- src/app/components/RightPane/SignUp.jsx
+++ src/app/components/RightPane/SignUp.jsx
@@ -67,7 +67,7 @@ const SignUp = ({ setRightComponent, setLeftComponent }) => {
             id="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             placeholder="Enter your name"
-            value={name}
+            value={userName}
             onChange={(e) => setUserName(e.target.value)}
             required
           />
