548a40614e1f20fd70e6b2fac4a88e6ed9f6cd34
SahanHeshan <dshsamarathunga@gmail.com>
2024-12-13 12:18:34 +0530

    profile-image  added

--- src/app/components/RightPane/UserSession.jsx
+++ src/app/components/RightPane/UserSession.jsx
@@ -8,27 +8,34 @@ const UserSession = () => {
   };
 
   return (
-    <>
-      <button 
-        type="button"  
+    <div className="relative">
+      {/* Dropdown Button */}
+      <button
+        type="button"
         id="dropdownInformationButton"
+        aria-expanded={isDropdownOpen}
+        aria-controls="dropdownInformation"
         onClick={toggleDropdown}
-        className="text-white  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        <img className="w-8 h-8 rounded-full" src="profile.svg" alt="user photo"/>
-        </button>
+        <img
+          className="w-8 h-8 rounded-full"
+          src="profile2.png"
+          alt="user photo"
+        />
+      </button>
 
+      {/* Dropdown Menu */}
       <div
         id="dropdownInformation"
-        className={`z-40 ${
+        className={`absolute right-0 mt-2 z-40 ${
           isDropdownOpen ? "block" : "hidden"
-        } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
+        }  border-3 bg-white divide-y divide-gray-300 rounded-lg shadow-lg w-44 dark:bg-gray-700 dark:divide-gray-600`}
       >
         <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
           <div>Bonnie Green</div>
           <div className="font-medium truncate">name@flowbite.com</div>
         </div>
-
         <div className="py-2">
           <a
             href="#"
@@ -38,7 +45,7 @@ const UserSession = () => {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
