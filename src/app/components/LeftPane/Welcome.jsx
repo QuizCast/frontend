62ce75f3e90e0b05645ba808a3c375797ee016d4
Lakith Randula <110283259+GLRandula@users.noreply.github.com>
2024-12-14 02:05:05 +0530

    Update the logo design

--- src/app/components/LeftPane/Welcome.jsx
+++ src/app/components/LeftPane/Welcome.jsx
@@ -6,18 +6,15 @@ const Welcome = ({ setRightComponent, setLeftComponent }) => {
             <div className="w-full p-4">
             <a
           href="#"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-6 rtl:space-x-reverse"
         >
           <img
-            src="logo2.png"
-            className="h-20"
+            src="logo.png"
+            className="h-36"
             alt="Flowbite Logo"
           />
-          <span className="hidden sm:block max-w-screen-xl self-center text-6xl font-semibold whitespace-nowrap dark:text-white">
-            Quiz
-            </span>
         </a>
-            <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-6 mt-8">Welcome!</h1>
+            <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-6 mt-4">Welcome!</h1>
             <p className="text-lg font-normal text-gray-900 dark:text-gray-400 mb-6">"Quiz" is an innovative platform for scheduling online quiz competitions, offering real-time interaction and a seamless, engaging experience.</p>
             <a href="#" className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Read more
