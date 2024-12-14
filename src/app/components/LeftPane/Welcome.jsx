4044db34a70bcc0ba61f913133a2b783c4be263a
Lakith Randula <110283259+GLRandula@users.noreply.github.com>
2024-12-14 09:44:49 +0530

    Logo updated

--- src/app/components/LeftPane/Welcome.jsx
+++ src/app/components/LeftPane/Welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = ({ setRightComponent, setLeftComponent }) => {
           />
         </a>
             <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-6">Welcome!</h1>
-            <p className="text-lg font-normal text-gray-900 dark:text-gray-400 mb-6">"Quiz" is an innovative platform for scheduling online quiz competitions, offering real-time interaction and a seamless, engaging experience.</p>
+            <p className="text-lg font-normal text-gray-900 dark:text-gray-400 mb-6">"QuizCast" is an innovative platform for scheduling online quiz competitions, offering real-time interaction and a seamless, engaging experience.</p>
             <a 
             className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 cursor-pointer"
             onClick={() => setRightComponent("AboutUs")}
@@ -25,32 +25,6 @@ const Welcome = ({ setRightComponent, setLeftComponent }) => {
                 </svg>
             </a>
             </div>
-
-
-        {/* Action Button */}
-        <div className="flex justify-center">
-          <a
-            href="#"
-            className="inline-flex items-center py-2.5 px-5 text-base font-medium text-white bg-slate-900 rounded-lg hover:bg-slate-950 focus:ring-4 focus:ring-blue-300 "
-          >
-            Read more
-            <svg
-              className="w-3.5 h-3.5 ml-2 rtl:rotate-180"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-              aria-hidden="true"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
-        </div>
       </div>
   );
 };
